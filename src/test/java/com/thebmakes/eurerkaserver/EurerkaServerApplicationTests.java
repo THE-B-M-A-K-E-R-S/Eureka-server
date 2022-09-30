93178193b1dc85db9187e63a328417c0810bf45b
BASSEM <bassem.jadoui@esprit.tn>
2022-09-30 13:29:53 +0100

    setting up eureka server ✅

---
+++ src/test/java/com/thebmakes/eurerkaserver/EurerkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebmakes.eurerkaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurerkaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
